d53ff4521ba79b75a1b8ad765929cf11ec3b13d7
soya365 <soyadt@gmail.com>
2016-07-04 13:29:51 +0800

    create some html files

--- Application/Owner/Controller/TeachingController.class.php
+++ Application/Owner/Controller/TeachingController.class.php
@@ -12,11 +12,39 @@ class TeachingController extends BaseController
 
     public function index()
     {
-        $this->redirect();
+        die('Error !');
     }
 
     public function view(string $table, int $bid)
     {
+        switch ($table) {
+            case 'student':
+                $map['member_id'] = session('UID');
+                $results = $this->teachStudent->where($map)->select();
+                break;
+            case 'class':
+                break;
+            case 'subject':
+                break;
+            case 'priced':
+                break;
+            case 'exam':
+                break;
+            case 'hours':
+                break;
+            case 'transfer':
+                break;
+            case 'course':
+                break;
+            case 'timetable':
+                break;
+
+            default:
+                die('Error !');
+                break;
+        }
+
+        $this->assign('results', $results);
         $this->display('view-' . $table);
     }
 }
